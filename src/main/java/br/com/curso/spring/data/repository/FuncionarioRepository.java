19280cdd5e558b38d7cac41d8bd532c6cc0505ea
Mariane <ma_ap_domingues@hotmail.com>
2021-12-13 23:18:54 -0300

    Adição de novos Derived Queries e JPQL

--- src/main/java/br/com/curso/spring/data/repository/FuncionarioRepository.java
+++ src/main/java/br/com/curso/spring/data/repository/FuncionarioRepository.java
@@ -1,13 +1,43 @@
 package br.com.curso.spring.data.repository;
 
 import br.com.curso.spring.data.model.entity.FuncionarioEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface FuncionarioRepository extends CrudRepository<FuncionarioEntity, Integer> {
 
     List<FuncionarioEntity> findByNome(String pNome);
+
+    //esta procurando funcionários pela descrição do cargo
+    List<FuncionarioEntity> findByCargoDescricao(String pDescricao);
+
+    @Query("SELECT f" +
+            " FROM FuncionarioEntity f" +
+            " JOIN f.cargo c" +
+            " WHERE c.descricao = :pDescricao")
+    List<FuncionarioEntity> findCargoDescricao(String pDescricao);
+
+    //Derived Query Methods
+    List<FuncionarioEntity> findByNomeAndSalarioGreaterThanAndDataContratacao(String pNome, double pSalario, LocalDate pDataContratacao);
+
+    //JPQL
+    @Query("SELECT f" +
+            " FROM FuncionarioEntity AS f" +
+            " WHERE f.nome = :pNome" +
+            " AND f.salario = :pSalario" +
+            " AND f.dataContratacao = :pDataContratacao")
+    List<FuncionarioEntity> findNomeSalarioMaiorDataContratacao(String pNome, double pSalario, LocalDate pDataContratacao);
+
+    List<FuncionarioEntity> findByVUnidadeTrabalho_Descricao(String pDescricao);
+
+    @Query("SELECT f" +
+            " FROM FuncionarioEntity f" +
+            " JOIN f.vUnidadeTrabalho u" +
+            " WHERE u.descricao = :pDescricao")
+    List<FuncionarioEntity> findUnidadeTrabalhos_Descricao(String pDescricao);
 }
